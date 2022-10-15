ccfba751677e4c8a4fe2eb41eb58b39121f1f4fd
removed0 <108957955+removed0@users.noreply.github.com108957955+removed0@users.noreply.github.com>
2022-10-15 19:53:07 +0300

    no newline
    Signed-off-by: removed0 <108957955+removed0@users.noreply.github.com108957955+removed0@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ fn main() {
     let mut input = String::new();
     println!("enter smth");
     io::stdin().read_line(&mut input).unwrap();
-    println!("++ +{}+ ++", input);
+
+    if input.ends_with('\n') {
+        input.pop();
+        if input.ends_with('\r') {
+            input.pop();
+        }
+    }
+    println!("++ - {} - ++", input);
 }
