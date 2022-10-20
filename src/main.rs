e8262e503438905ebbca28d5880102399f864244
removed0 <removed@tuta.io>
2022-10-20 22:00:54 +0300

    why is cpp so much easier

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use std::io;
+use std::num::ParseIntError;
 
 mod check_q;
 mod one;
 mod two;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut input = String::new();
     loop {
         println!("type quit to exit");
         println!("type one or two");
         io::stdin().read_line(&mut input).unwrap();
         if check_q::check_q(&mut input) {
-            return;
+            return Ok(());
         }
 
         if input == "one" || input == "1" {
             one::one();
         } else if input == "two" || input == "2" {
-            two::two();
+            two::two()?;
         }
         input.clear();
     }
--- src/two.rs
+++ src/two.rs
@@ -1,27 +1,28 @@
 use super::check_q::check_q;
 use std::io;
+use std::num::ParseIntError;
 
-pub fn two() {
+pub fn two() -> Result<(), ParseIntError> {
     let mut input = String::new();
     loop {
         println!("enter the first number");
 
         io::stdin().read_line(&mut input).unwrap();
         if check_q(&mut input) {
-            return;
+            return Ok(());
         }
 
-        let first: i32 = input.parse().unwrap();
+        let first: i32 = input.parse()?;
         input.clear();
 
         println!("enter the second number");
 
         io::stdin().read_line(&mut input).unwrap();
         if check_q(&mut input) {
-            return;
+            return Ok(());
         }
 
-        let second: i32 = input.parse().unwrap();
+        let second: i32 = input.parse()?;
 
         println!("{} + {} = {}", first, second, first + second);
         input.clear();
